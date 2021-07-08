0a5fd7eff198caae610885daf1fd564408ccc5d8
RichardBanh <20131326+RichardBanh@users.noreply.github.com>
2021-07-07 23:36:49 -0700

    working on the menu bar

--- App/frontend/src/Components/Workingscreen.jsx
+++ App/frontend/src/Components/Workingscreen.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 
-
 //auto save function?
 export const WorkingScreen = () => {
-  
-
   return (
     <>
-      <div>Working screen!</div>
+      <div className="content">
+        <div className="side_left">
+          <div className="menubar">
+            <div>Logo</div>
+          </div>
+          <div className="rec_blk"></div>
+          <div className="rec_blk"></div>
+          <div className="rec_blk"></div>
+          <div className="rec_blk"></div>
+          <div className="rec_blk"></div>
+          <div className="rec_blk"></div>
+        </div>
+        <div className="side_right"></div>
+      </div>
     </>
   );
 };
